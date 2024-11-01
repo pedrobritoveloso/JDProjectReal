20ad28571ebd590fa1d96a95b595f794bb46e7ce
pedrobritoveloso <up202005801@up.pt>
2024-11-01 17:02:38 +0000

    Change ending position and changed gameEnding Script so it works with the key

--- Assets/UnityTechnologies/Scripts/GameEnding.cs
+++ Assets/UnityTechnologies/Scripts/GameEnding.cs
@@ -5,51 +5,79 @@ using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
-  public float fadeDuration = 1f;
-  public float displayImageDuration = 1f;
-  public GameObject player;
-  public CanvasGroup exitBackgroundImageCanvasGroup; /*This will store a reference to the CanvasGroup
-   component of the exitBackgroundImage GameObject.*/
-  public AudioSource exitAudio;
-  public CanvasGroup caughtBackgroundImageCanvasGroup; /*This will store a reference to the CanvasGroup
-   component of the caughtBackgroundImage GameObject.*/
-  public AudioSource caughtAudio;
-  bool m_IsPlayerAtExit;
-  bool m_IsPlayerCaught;
-  float m_Timer;
-  bool m_HasAudioPlayed;
-  void OnTriggerEnter(Collider other){ 
-    if(other.gameObject == player){
-        m_IsPlayerAtExit = true;
-    }    
-  }
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup; /*This will store a reference to the CanvasGroup
+    component of the exitBackgroundImage GameObject.*/
+    public AudioSource exitAudio;
+    public CanvasGroup caughtBackgroundImageCanvasGroup; /*This will store a reference to the CanvasGroup
+    component of the caughtBackgroundImage GameObject.*/
+    public AudioSource caughtAudio;
 
-  public void CaughtPlayer ()
-    {
-        m_IsPlayerCaught = true;
+    /*public GameObject m_ClueBed;
+    public GameObject m_ClueTable;
+    public GameObject m_ClueBath;
+    public GameObject m_Key;*/
+    
+    bool m_KeyFound;
+
+    bool m_ClueBedFound;
+    bool m_ClueTableFound;
+    bool m_ClueBathFound;
+    bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
+    float m_Timer;
+    bool m_HasAudioPlayed;
+    void OnTriggerEnter(Collider other){ 
+        if(other.gameObject == player){
+            /*if (other.gameObject == m_ClueBed) {
+                m_ClueBedFound = true;
+                m_ClueBed.SetActive(false);
+                m_ClueTable.SetActive(true);
+            } else if (other.gameObject == m_ClueTable && m_ClueBedFound) {
+                m_ClueTableFound = true;
+                m_ClueTable.SetActive(false);
+                m_ClueBath.SetActive(true);
+            } else if (other.gameObject == m_ClueBath && m_ClueTableFound) {
+                m_ClueBathFound = true;
+                m_ClueBath.SetActive(false);
+                m_Key.SetActive(true);
+            } else if (other.gameObject == m_Key && m_ClueBathFound) {
+                m_KeyFound = true;
+                m_Key.SetActive(false);
+            } else if (other.gameObject == player && m_KeyFound) {*/
+                m_IsPlayerAtExit = true;
+            //}
+        }  
     }
-  void Update(){
-    if(m_IsPlayerAtExit){ //This will check if the player is at the exit
-        EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
-    }else if(m_IsPlayerCaught){ //This will check if the player is caught
-        EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
-    }
-  }
-    void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource){
-        if (!m_HasAudioPlayed){
-            audioSource.Play();
-            m_HasAudioPlayed = true;
+
+    public void CaughtPlayer ()
+        {
+            m_IsPlayerCaught = true;
         }
-        m_Timer += Time.deltaTime;
-        imageCanvasGroup.alpha = m_Timer / fadeDuration;
-        if(m_Timer > fadeDuration + displayImageDuration){
-            if(doRestart){
-                SceneManager.LoadScene (0);
-            }
-            else
-            {
-                Application.Quit ();
-            }
+    void Update(){
+        if(m_IsPlayerAtExit){ //This will check if the player is at the exit
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
+        }else if(m_IsPlayerCaught){ //This will check if the player is caught
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
     }
+        void EndLevel (CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource){
+            if (!m_HasAudioPlayed){
+                audioSource.Play();
+                m_HasAudioPlayed = true;
+            }
+            m_Timer += Time.deltaTime;
+            imageCanvasGroup.alpha = m_Timer / fadeDuration;
+            if(m_Timer > fadeDuration + displayImageDuration){
+                if(doRestart){
+                    SceneManager.LoadScene (0);
+                }
+                else
+                {
+                    Application.Quit ();
+                }
+            }
+        }
 }
